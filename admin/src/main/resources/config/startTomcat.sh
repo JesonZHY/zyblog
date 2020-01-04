d67947ee4b5b2ccb1485091c848ee581d980d5e1
Alex <503816801@qq.com>
2020-01-04 15:30:29 +0800

    修改部署shell代码

--- admin/src/main/resources/config/startTomcat.sh
+++ admin/src/main/resources/config/startTomcat.sh
@@ -2,6 +2,8 @@
 
 export JAVA_HOME=/usr/local/java
 export PATH=$JAVA_HOME/bin:$PATH
+cd /usr/local/tomcat/webapps
+rm -rf ./*
 cd /home/admin
 chmod 777 ./*
 cp admin.war /usr/local/tomcat/webapps/
